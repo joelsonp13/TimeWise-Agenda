9a5694509bee26256cc62de8dfad243f6348a66f
devyuri <159211132+YuriTurola@users.noreply.github.com>
2024-09-18 21:15:00 -0300

    Update LoginTimeWise.js

--- firebase/public/js/LoginTimeWise.js
+++ firebase/public/js/LoginTimeWise.js
@@ -132,7 +132,7 @@ firebase.auth().onAuthStateChanged((user) => {
             window.location.href = 'returnUrl';
         } else {
             // Se não houver returnUrl, redirecione para uma página padrão
-            window.location.href = 'returnUrl';
+            
         }
     }
 });
